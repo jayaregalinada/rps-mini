365e2a7c921bd3c764f1aa32899d926332b1c4d2
Jay Are Galinada <jayaregalinada@gmail.com>
2025-07-26 01:43:29 +0800

    fix: added HOST

--- src/services/match-service.js
+++ src/services/match-service.js
@@ -1,8 +1,9 @@
 export async function matchService([playerWeapon, opponentWeapon]) {
+  const URL = `/api/v1/match?object_one=${playerWeapon}&object_two=${opponentWeapon}`;
+  const HOST = import.meta.env.PROD && 'https://rps101.pythonanywhere.com';
+
   try {
-    const response = await fetch(
-      `/api/v1/match?object_one=${playerWeapon}&object_two=${opponentWeapon}`
-    );
+    const response = await fetch(HOST + URL);
 
     if (!response.ok) {
       throw new Error(`Failed with status: ${response.status}`);
